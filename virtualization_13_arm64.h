b4aa52663315118f4c9211504c33b891c37dcf17
Sergei Egorov <bsideup@gmail.com>
2023-09-13 12:28:09 -0400

    Update virtualization_13_arm64.h

--- virtualization_13_arm64.h
+++ virtualization_13_arm64.h
@@ -6,7 +6,7 @@
 
 #pragma once
 
-#ifdef __arm64__
+// #ifdef __arm64__
 
 // FIXME(codehex): this is dirty hack to avoid clang-format error like below
 // "Configuration file(s) do(es) not support C++: /github.com/Code-Hex/vz/.clang-format"
@@ -26,4 +26,4 @@ void *newVZMacOSVirtualMachineStartOptions(bool startUpFromMacOSRecovery);
 
 void *newVZMacTrackpadConfiguration();
 
-#endif
+// #endif
